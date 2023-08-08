6d2a70e8464d312d3f701dd3028cd850c40b9c22
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-08-08 15:15:33 +0700

    baru

--- app.js
+++ app.js
@@ -12,22 +12,7 @@ let logout = require('./routes/logout');
 
 let app = express();
 
-// Tentukan asal yang diizinkan
-const allowedOrigins = ['http://localhost:3000'];
-
-// Konfigurasi opsi CORS
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
