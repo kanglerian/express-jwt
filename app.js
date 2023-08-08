72518647a49ebcd59cbe06df975f0523e2695a07
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-08-08 15:55:55 +0700

    cors

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ let logout = require('./routes/logout');
 
 let app = express();
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
